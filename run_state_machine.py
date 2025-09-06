7f6dd8dca713200823cf317108d66780b2d547a1
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-09-06 09:11:37 +0300

    Bugfix class Connection
    Replace __getattr__ with explicit methods: open(), read(), ...

--- run_state_machine.py
+++ run_state_machine.py
@@ -5,21 +5,13 @@
 >>> c = Connection()
 >>> c._state
 <class '__main__.ClosedConnectionState'>
->>> c.read()
-Traceback (most recent call last):
-  File "<stdin>", line 1, in <module>
-  File "example.py", line 10, in read
-    return self._state.read(self)
-  File "example.py", line 43, in read
-    raise RuntimeError('Not open')
-RuntimeError: Not open
 >>> c.open()
 >>> c._state
 <class '__main__.OpenConnectionState'>
 >>> c.read()
 reading
->>> c.write('hello')
-writing
+>>> c.read()
+reading
 >>> c.close()
 >>> c._state
 <class '__main__.ClosedConnectionState'>
@@ -35,12 +27,17 @@ class Connection:
     def change_state(self, state):
         self._state = state
 
-    def __getattr__(self, name):
-        if name in ("open", "read", "write", "close"):
-            return getattr(self._state, name)(self._state)
-        raise AttributeError(
-            "{!r} object has no attribute {!r}".format(type(self), name)
-        )
+    def open(self):
+        self._state.open(self)
+
+    def read(self):
+        self._state.read(self)
+
+    def write(self):
+        self._state.write(self)
+
+    def close(self):
+        self._state.close(self)
 
 
 class ConnectionState:
@@ -63,7 +60,8 @@ class ConnectionState:
 
 class ClosedConnectionState(ConnectionState):
     @staticmethod
-    def open(conn):
+    def open(conn: Connection):
+        assert isinstance(conn, Connection)
         conn.change_state(OpenConnectionState)
 
 
