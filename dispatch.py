1a4dbe81027274d901afefcba111db27a92462d5
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-09-07 21:21:06 +0300

    Refactor all methods
    -        if self.state == ConnectionState.CLOSED:
    . .
    +        if self.state != ConnectionState.CLOSED:
                 raise ConnectionStateError("already open")

--- dispatch.py
+++ dispatch.py
@@ -3,10 +3,22 @@
 # PYTHON_ARGCOMPLETE_OK
 """
 >>> conn = Connection()
+>>> conn.close()
+Traceback (most recent call last):
+...
+ConnectionStateError: connection already closed
+>>> conn.read()
+Traceback (most recent call last):
+...
+ConnectionStateError: connection not opened
 >>> conn.open()
 opened
 >>> conn.read()
 reading
+>>> conn.write()
+writing
+>>> conn.close()
+connection closed
 """
 from enum import Enum
 
@@ -23,30 +35,25 @@ class Connection:
         self.state: ConnectionState = ConnectionState.CLOSED
 
     def open(self):
-        if self.state == ConnectionState.CLOSED:
-            self.state = ConnectionState.OPEN
-            print("opened")
-        else:
+        if self.state != ConnectionState.CLOSED:
             raise ConnectionStateError("already open")
+        self.state = ConnectionState.OPEN
+        print("opened")
 
     def read(self):
-        if self.state == ConnectionState.OPEN:
-            print("reading")
-        else:
+        if self.state != ConnectionState.OPEN:
             raise ConnectionStateError("connection not opened")
+        print("reading")
 
     def write(self):
-        if self.state == ConnectionState.OPEN:
-            print("reading")
-        else:
+        if self.state != ConnectionState.OPEN:
             raise ConnectionStateError("connection not opened")
+        print("writing")
 
     def close(self):
-        if self.state == ConnectionState.OPEN:
-            print("close connection")
-            self.state == ConnectionState.CLOSED
-        else:
-            raise ConnectionStateError("can't close closed connection")
+        if self.state != ConnectionState.OPEN:
+            raise ConnectionStateError("connection already closed")
+        print("connection closed")
 
 
 if __name__ == "__main__":
